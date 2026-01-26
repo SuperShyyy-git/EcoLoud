ed953afc1367a5746be847d10e59013c08625809
SuperShyyy-git <adrianavorque052504@gmail.com>
2026-01-26 11:03:26 +0800

    Fix deployment paths: run manage.py and gunicorn from subdirectory

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+# Change directory to where manage.py is located
+cd ecoaware_ph
+
 # Convert static asset files
 python manage.py collectstatic --no-input
 
